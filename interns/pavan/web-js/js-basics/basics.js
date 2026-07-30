3707dced025a200828b897e62ce95f80cd06fd8e
Pavan-uxdl <pavan@uxdl.in>
2026-07-30 23:32:12 +0530

    created js-basics

---
+++ interns/pavan/web-js/js-basics/basics.js
@@ -0,0 +1,14 @@
+const userName = "paul";
+let score = 10;
+
+const skills = ['Html','css','JavaScript'];
+
+const details = {
+    name : 'Paul',
+    role : 'Intern'
+}
+
+console.log(`My name is ${userName}`);
+console.log(`My Score is ${score}`);
+console.log(`Im learning ${skills.join(", ")}`);
+console.log(`Role ${details.role}`);
